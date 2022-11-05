0054c39e9e7dacc105ed75fb2983a4ad3386fcc0
jonathan martins <jonathan.martins@pipetech.com.br>
2022-11-05 14:55:21 -0300

    try

---
+++ dist/routes.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateCourse = void 0;
+const CreateCourseService_1 = __importDefault(require("./CreateCourseService"));
+function CreateCourse(request, response) {
+    CreateCourseService_1.default.execute("NodeJs", 10, "jhon");
+}
+exports.CreateCourse = CreateCourse;
